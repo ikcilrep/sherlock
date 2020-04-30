9e44ade88797be83d5ec159beab0f7e78280caa4
ikcilrep <frajerzycki@protonmail.com>
2020-04-30 22:36:29 +0200

    Check if bishop is still on board before adding move

--- src/pieces/bishop.rs
+++ src/pieces/bishop.rs
@@ -9,22 +9,22 @@ pub fn generate_pseudo_legal_moves(from: usize, board: &Board, result: &mut Vec<
     let from_file = signed_from & 7;
     let mut to = signed_from + 9;
 
-    while to & 7 > from_file && add_sliding_move(from, to, bishop_color, result, board) {
+    while to < 64 && to & 7 > from_file && add_sliding_move(from, to, bishop_color, result, board) {
         to += 9;
     }
 
     to = signed_from - 9;
-    while to & 7 > from_file && add_sliding_move(from, to, bishop_color, result, board) {
+    while to > 0 && to & 7 > from_file && add_sliding_move(from, to, bishop_color, result, board) {
         to -= 9;
     }
 
     to = signed_from + 7;
-    while to & 7 < from_file && add_sliding_move(from, to, bishop_color, result, board) {
+    while to < 64 && to & 7 < from_file && add_sliding_move(from, to, bishop_color, result, board) {
         to += 7;
     }
 
     to = signed_from - 7;
-    while to & 7 < from_file && add_sliding_move(from, to, bishop_color, result, board) {
+    while to > 0 && to & 7 < from_file && add_sliding_move(from, to, bishop_color, result, board) {
         to -= 7;
     }
 }
