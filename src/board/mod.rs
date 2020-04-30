5c98e76402ce9d9e668c9fb2600a818160a7949f
ikcilrep <frajerzycki@protonmail.com>
2020-04-30 21:39:37 +0200

    Add make_move function

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,3 +1,7 @@
+use crate::moves::{
+    get_from, get_move_type, get_moved_piece, get_promoted_piece, get_to, Move,
+    CASTLING_KINGS_SIDE, CASTLING_QUEENS_SIDE, EN_PASSANT,
+};
 use crate::pieces::color::{get_piece_color, Color};
 use crate::pieces::{ColorizedPiece, EMPTY_SQUARE};
 
@@ -14,6 +18,14 @@ pub struct Board {
     pub en_passant_square: i8,
 }
 
+const KING_POSITIONS: [usize; 2] = [4, 60];
+const KINGS_ROOKS_POSITIONS: [usize; 2] = [7, 63];
+const QUEENS_ROOKS_POSITIONS: [usize; 2] = [0, 56];
+
+const KINGS_ROOKS_AFTER_CASTLING_POSITIONS: [usize; 2] = [5, 61];
+const QUEENS_ROOKS_AFTER_CASTLING_POSITIONS: [usize; 2] = [3, 59];
+const INVERSED_PAWN_STEPS: [i8; 2] = [-8, 8];
+
 impl Board {
     #[inline]
     pub fn can_be_moved(self: &Board, to: i8, piece_to_move_color: Color) -> bool {
@@ -25,5 +37,35 @@ impl Board {
     pub fn can_capture(self: &Board, to: i8, piece_to_move_color: Color) -> bool {
         self.pieces[to as usize] != EMPTY_SQUARE
             && get_piece_color(self.pieces[to as usize]) != piece_to_move_color
+    }
+
+    #[inline]
+    pub fn make_move(self: &mut Board, half_move: Move) {
+        let from = get_from(half_move);
+        let to = get_to(half_move);
+        self.pieces[to] = get_promoted_piece(half_move);
+        self.pieces[from] = EMPTY_SQUARE;
+        let color = get_piece_color(get_moved_piece(half_move)) as usize;
+        self.has_king_stayed_in_place[color] &= KING_POSITIONS[color] != from;
+        self.has_kings_rook_stayed_in_place[color] &= KINGS_ROOKS_POSITIONS[color] != from;
+        self.has_queens_rook_stayed_in_place[color] &= QUEENS_ROOKS_POSITIONS[color] != from;
+
+        match get_move_type(half_move) {
+            CASTLING_KINGS_SIDE => {
+                self.pieces[KINGS_ROOKS_AFTER_CASTLING_POSITIONS[color]] =
+                    self.pieces[KINGS_ROOKS_POSITIONS[color]];
+                self.pieces[KINGS_ROOKS_POSITIONS[color]] = EMPTY_SQUARE;
+            }
+            CASTLING_QUEENS_SIDE => {
+                self.pieces[QUEENS_ROOKS_AFTER_CASTLING_POSITIONS[color]] =
+                    self.pieces[QUEENS_ROOKS_POSITIONS[color]];
+                self.pieces[QUEENS_ROOKS_POSITIONS[color]] = EMPTY_SQUARE;
+            }
+            EN_PASSANT => {
+                self.pieces[(to as i8 + INVERSED_PAWN_STEPS[color]) as usize] = EMPTY_SQUARE;
+            }
+            _ => {}
+        }
+        self.side = !self.side;
     }
 }
