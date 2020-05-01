203a0c296b35efafcae00280fe70595d1830cdb9
ikcilrep <frajerzycki@protonmail.com>
2020-05-02 00:07:19 +0200

    Create king move validation functions

--- src/pieces/king.rs
+++ src/pieces/king.rs
@@ -1,7 +1,47 @@
 use crate::board::Board;
 use crate::moves::constructors::{new_castling, new_move};
 use crate::moves::{Move, CASTLING_KINGS_SIDE, CASTLING_QUEENS_SIDE};
-use crate::pieces::color::get_piece_color;
+use crate::pieces::color::{get_piece_color, Color};
+
+#[inline]
+fn is_move_northwest_pseudo_legal(from_file: i8, to: i8, board: &Board, king_color: Color) -> bool {
+    to < 64 && to & 7 < from_file && board.can_be_moved(to, king_color)
+}
+
+#[inline]
+fn is_move_north_pseudo_legal(_: i8, to: i8, board: &Board, king_color: Color) -> bool {
+    to < 64 && board.can_be_moved(to, king_color)
+}
+
+#[inline]
+fn is_move_northeast_pseudo_legal(from_file: i8, to: i8, board: &Board, king_color: Color) -> bool {
+    to < 64 && to & 7 > from_file && board.can_be_moved(to, king_color)
+}
+
+#[inline]
+fn is_move_east_pseudo_legal(from_file: i8, to: i8, board: &Board, king_color: Color) -> bool {
+    to & 7 > from_file && board.can_be_moved(to, king_color)
+}
+
+#[inline]
+fn is_move_southeast_pseudo_legal(from_file: i8, to: i8, board: &Board, king_color: Color) -> bool {
+    to >= 0 && to & 7 > from_file && board.can_be_moved(to, king_color)
+}
+
+#[inline]
+fn is_move_south_pseudo_legal(_: i8, to: i8, board: &Board, king_color: Color) -> bool {
+    to >= 0 && board.can_be_moved(to, king_color)
+}
+
+#[inline]
+fn is_move_southwest_pseudo_legal(from_file: i8, to: i8, board: &Board, king_color: Color) -> bool {
+    to >= 0 && to & 7 < from_file && board.can_be_moved(to, king_color)
+}
+
+#[inline]
+fn is_move_west_pseudo_legal(from_file: i8, to: i8, board: &Board, king_color: Color) -> bool {
+    to & 7 < from_file && board.can_be_moved(to, king_color)
+}
 
 pub fn generate_pseudo_legal_moves(from: usize, board: &Board, result: &mut Vec<Move>) {
     let signed_from = from as i8;
@@ -9,42 +49,42 @@ pub fn generate_pseudo_legal_moves(from: usize, board: &Board, result: &mut Vec<
     let king = board.pieces[from];
     let king_color = get_piece_color(king);
     let mut to = signed_from + 7;
-    if to < 64 && to & 7 < from_file && board.can_be_moved(to, king_color) {
-        result.push(new_move(from, to, board));
+    if is_move_northwest_pseudo_legal(from_file, to, board, king_color) {
+        result.push(new_move(from, to, board))
     }
 
     to = signed_from + 8;
-    if to < 64 && board.can_be_moved(to, king_color) {
+    if is_move_north_pseudo_legal(from_file, to, board, king_color) {
         result.push(new_move(from, to, board));
     }
 
     to = signed_from + 9;
-    if to < 64 && to & 7 > from_file && board.can_be_moved(to, king_color) {
+    if is_move_northeast_pseudo_legal(from_file, to, board, king_color) {
         result.push(new_move(from, to, board));
     }
 
     to = signed_from + 1;
-    if to & 7 > from_file && board.can_be_moved(to, king_color) {
+    if is_move_east_pseudo_legal(from_file, to, board, king_color) {
         result.push(new_move(from, to, board));
     }
 
     to = signed_from - 7;
-    if to >= 0 && to & 7 > from_file && board.can_be_moved(to, king_color) {
+    if is_move_southeast_pseudo_legal(from_file, to, board, king_color) {
         result.push(new_move(from, to, board));
     }
 
     to = signed_from - 8;
-    if to >= 0 && board.can_be_moved(to, king_color) {
+    if is_move_south_pseudo_legal(from_file, to, board, king_color) {
         result.push(new_move(from, to, board));
     }
 
     to = signed_from - 9;
-    if to >= 0 && to & 7 < from_file && board.can_be_moved(to, king_color) {
+    if is_move_southwest_pseudo_legal(from_file, to, board, king_color) {
         result.push(new_move(from, to, board));
     }
 
     to = signed_from - 1;
-    if to & 7 < from_file && board.can_be_moved(to, king_color) {
+    if is_move_west_pseudo_legal(from_file, to, board, king_color) {
         result.push(new_move(from, to, board));
     }
 
