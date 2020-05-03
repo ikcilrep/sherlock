35cbeb382e15326132732f4cd51d2e74346c2674
ikcilrep <frajerzycki@protonmail.com>
2020-05-04 01:16:10 +0200

    Split move generation into many functions

--- src/pieces/rook.rs
+++ src/pieces/rook.rs
@@ -1,28 +1,67 @@
+extern crate rand;
+
 use crate::board::Board;
 use crate::moves::Move;
-use crate::pieces::color::get_piece_color;
+use crate::pieces::color::{get_piece_color, Color};
 use crate::pieces::sliders::add_sliding_move;
+use rand::rngs::ThreadRng;
 
-pub fn generate_pseudo_legal_moves(from: usize, board: &Board, result: &mut Vec<Move>) {
-    let rook_color = get_piece_color(board.pieces[from]);
-    let signed_from = from as i8;
-    let mut to = signed_from + 1;
+#[inline]
+fn generate_pseudo_legal_moves_on_east(
+    from: usize,
+    board: &Board,
+    rook_color: Color,
+    result: &mut Vec<Move>,
+) {
+    let mut to = from as i8 + 1;
     while to & 7 != 0 && add_sliding_move(from, to, rook_color, result, board) {
         to += 1;
     }
+}
 
-    to = signed_from - 1;
+#[inline]
+fn generate_pseudo_legal_moves_on_west(
+    from: usize,
+    board: &Board,
+    rook_color: Color,
+    result: &mut Vec<Move>,
+) {
+    let mut to = from as i8 - 1;
     while to & 7 != 7 && add_sliding_move(from, to, rook_color, result, board) {
         to -= 1;
     }
+}
 
-    to = signed_from + 8;
+#[inline]
+fn generate_pseudo_legal_moves_on_north(
+    from: usize,
+    board: &Board,
+    rook_color: Color,
+    result: &mut Vec<Move>,
+) {
+    let mut to = from as i8 + 8;
     while to < 64 && add_sliding_move(from, to, rook_color, result, board) {
         to += 8;
     }
+}
 
-    to = signed_from - 8;
+#[inline]
+fn generate_pseudo_legal_moves_on_south(
+    from: usize,
+    board: &Board,
+    rook_color: Color,
+    result: &mut Vec<Move>,
+) {
+    let mut to = from as i8 - 8;
     while to >= 0 && add_sliding_move(from, to, rook_color, result, board) {
         to -= 8;
     }
+}
+
+pub fn generate_pseudo_legal_moves(from: usize, board: &Board, result: &mut Vec<Move>) {
+    let rook_color = get_piece_color(board.pieces[from]);
+    generate_pseudo_legal_moves_on_east(from, board, rook_color, result);
+    generate_pseudo_legal_moves_on_west(from, board, rook_color, result);
+    generate_pseudo_legal_moves_on_north(from, board, rook_color, result);
+    generate_pseudo_legal_moves_on_south(from, board, rook_color, result);
 }
