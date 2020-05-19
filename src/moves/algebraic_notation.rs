e3dfb7dd92822df41deb70fff3b450102ccfa8a3
ikcilrep <frajerzycki@protonmail.com>
2020-05-19 23:01:06 +0200

    Split to_algebraic_notation into smaller functions

--- src/moves/algebraic_notation.rs
+++ src/moves/algebraic_notation.rs
@@ -36,6 +36,33 @@ fn piece_to_char(piece: Piece) -> char {
     }
 }
 
+fn remove_ambiguities(half_move: Move, move_string_part: &mut String, board: &Board) {
+    let from = get_from(half_move) as i8;
+    let moved_piece = get_moved_piece(half_move);
+    let attacked_color = get_piece_color(moved_piece);
+    let to = get_to(half_move);
+    let attackers_locations =
+        board.get_pieces_of_type_attacking_square_locations(to as i8, moved_piece, attacked_color);
+
+    for attacker_location in attackers_locations {
+        if attacker_location != from {
+            let attacker_location_rank = attacker_location >> 3;
+            let from_rank = from >> 3;
+            let attacker_location_file = attacker_location & 7;
+            let from_file = from & 7;
+
+            if attacker_location_rank == from_rank {
+                move_string_part.push(file_to_char(from_file as u8));
+            } else if attacker_location_file == from_file {
+                move_string_part.push(rank_to_char(from_rank as u8));
+            } else {
+                move_string_part.push(file_to_char(from_file as u8));
+            }
+            break;
+        }
+    }
+}
+
 pub fn to_algebraic_notation(half_move: Move, board: &Board) -> String {
     match get_move_type(half_move) {
         CASTLING_KINGS_SIDE => String::from("O-O"),
@@ -44,35 +71,9 @@ pub fn to_algebraic_notation(half_move: Move, board: &Board) -> String {
             let mut result = String::new();
             let moved_piece = get_moved_piece(half_move);
             let uncolorized_moved_piece = uncolorize_piece(moved_piece);
-            let from = get_from(half_move) as i8;
-            let attacked_color = get_piece_color(moved_piece);
             if uncolorized_moved_piece != PAWN {
                 result.push(piece_to_char(uncolorized_moved_piece));
-
-                let to = get_to(half_move);
-                let attackers_locations = board.get_pieces_of_type_attacking_square_locations(
-                    to as i8,
-                    moved_piece,
-                    attacked_color,
-                );
-
-                for attacker_location in attackers_locations {
-                    if attacker_location != from {
-                        let attacker_location_rank = attacker_location >> 3;
-                        let from_rank = from >> 3;
-                        let attacker_location_file = attacker_location & 7;
-                        let from_file = from & 7;
-
-                        if attacker_location_rank == from_rank {
-                            result.push(file_to_char(from_file as u8));
-                        } else if attacker_location_file == from_file {
-                            result.push(rank_to_char(from_rank as u8));
-                        } else {
-                            result.push(file_to_char(from_file as u8));
-                        }
-                        break;
-                    }
-                }
+                remove_ambiguities(half_move, &mut result, board);
             }
             // Rank or file will be here.
             if get_captured_piece(half_move) != EMPTY_SQUARE {
