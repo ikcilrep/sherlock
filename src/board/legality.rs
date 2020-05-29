a6e87b1365d6cbcb935fdf32104a6a47cb6249e6
ikcilrep <frajerzycki@protonmail.com>
2020-05-29 20:31:02 +0200

    Fix and separate can_get_out_of_check on rank

--- src/board/legality.rs
+++ src/board/legality.rs
@@ -134,6 +134,30 @@ impl Board {
             )
     }
 
+    fn can_get_out_of_check_on_rank(
+        self: &mut Board,
+        attacker_location: i8,
+        king_location: i8,
+        color: Color,
+    ) -> bool {
+        fn is_defended(square: i8, king_location: i8, color: Color, board: &mut Board) -> bool {
+            pawn::can_be_moved_on_empty_square_without_capture(square, board)
+                || board.is_square_defended_not_from_rank_by_not_pawn(square, king_location, color)
+        }
+
+        pawn::can_capture_on_enemy_occupied_square(attacker_location, self)
+            || self.is_square_defended_not_from_rank_by_not_pawn(
+                attacker_location,
+                king_location,
+                color,
+            )
+            || (attacker_location > king_location
+                && (king_location..attacker_location)
+                    .any(|square| is_defended(square, king_location, color, self)))
+            || (attacker_location..king_location)
+                .any(|square| is_defended(square, king_location, color, self))
+    }
+
     fn can_get_out_of_check(
         self: &mut Board,
         king_attackers_locations: &Vec<i8>,
@@ -150,24 +174,12 @@ impl Board {
                 let king_location_rank = king_location >> 3;
 
                 if attacker_location_rank == king_location_rank {
-                    if attacker_location > king_location {
-                        return (king_location..attacker_location).any(|square| {
-                            pawn::can_be_moved_on_empty_square_without_capture(square, self)
-                                || self.is_square_defended_not_from_rank_by_not_pawn(
-                                    square,
-                                    king_location,
-                                    color,
-                                )
-                        }) || pawn::can_capture_on_enemy_occupied_square(
-                            attacker_location,
-                            self,
-                        ) || self.is_square_defended_not_from_rank_by_not_pawn(
-                            attacker_location,
-                            king_location,
-                            color,
-                        );
-                    }
-                } // file, diagonals, else in future
+                    return self.can_get_out_of_check_on_rank(
+                        attacker_location,
+                        king_location,
+                        color,
+                    );
+                } // diagonals, else in future
                 false
             }
             _ => king::can_be_moved(king_location as usize, self),
