65538e85d2b67a9aaff5df289107c03afe9070f7
ikcilrep <frajerzycki@protonmail.com>
2020-06-07 21:55:56 +0200

    Begin writing defender.locations module

--- src/board/defenders/locations.rs
+++ src/board/defenders/locations.rs
@@ -1,14 +1,10 @@
 use crate::board::Board;
 use crate::pieces::color::{colorize_piece, Color};
-use crate::pieces::knight;
-use crate::pieces::{ColorizedPiece, KNIGHT};
-
-pub mod diagonal;
-pub mod locations;
-pub mod straight_line;
+use crate::pieces::ColorizedPiece;
+use crate::pieces::{knight, KNIGHT};
 
 impl Board {
-    pub fn is_square_defended_by_slider(
+    pub fn get_slider_or_queen_defending_square_location(
         &mut self,
         square: i8,
         possible_attacker: ColorizedPiece,
@@ -16,7 +12,7 @@ impl Board {
         defended_color: Color,
         increment: i8,
         predicate: fn(i8, i8) -> bool,
-    ) -> bool {
+    ) -> i8 {
         let defender_location = self.get_slider_or_queen_attacking_square_location(
             square,
             possible_attacker,
@@ -24,16 +20,22 @@ impl Board {
             increment,
             predicate,
         );
-        defender_location > -1
+        return if defender_location > -1
             && !self.is_piece_pinned(defender_location, square, defended_piece_location)
+        {
+            defender_location
+        } else {
+            -1
+        };
     }
 
-    pub fn is_square_defended_by_knight(
+    pub fn get_knights_defending_square_locations(
         &mut self,
         square: i8,
         defended_piece_location: i8,
         defended_color: Color,
-    ) -> bool {
+        result: &mut Vec<i8>,
+    ) {
         let colorized_knight = colorize_piece(KNIGHT, defended_color);
 
         let square_file = square & 7;
@@ -42,10 +44,11 @@ impl Board {
         knight::MOVE_PSEUDO_LEGALITY_VALIDATORS
             .iter()
             .zip(moves_to.iter())
-            .any(|(is_move_pseudo_legal, defender_square)| {
-                is_move_pseudo_legal(square_file, *defender_square, self, defended_color)
-                    && self.state.pieces[*defender_square as usize] == colorized_knight
-                    && !self.is_piece_pinned(*defender_square, square, defended_piece_location)
+            .filter(|&(is_move_pseudo_legal, &defender_square)| {
+                is_move_pseudo_legal(square_file, defender_square, self, defended_color)
+                    && self.state.pieces[defender_square as usize] == colorized_knight
+                    && !self.is_piece_pinned(defender_square, square, defended_piece_location)
             })
+            .for_each(|(_, &defender_square)| result.push(defender_square));
     }
 }
