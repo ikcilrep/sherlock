589ceeceaa0956438ac951eded38cc584cf4b25e
ikcilrep <frajerzycki@protonmail.com>
2020-06-09 17:39:59 +0200

    Add getting defending pieces on diagonals

--- src/board/defenders/locations.rs
+++ src/board/defenders/locations.rs
@@ -1,7 +1,7 @@
 use crate::board::Board;
 use crate::pieces::color::{colorize_piece, Color};
 use crate::pieces::ColorizedPiece;
-use crate::pieces::{knight, KNIGHT, ROOK};
+use crate::pieces::{knight, BISHOP, KNIGHT, ROOK};
 
 impl Board {
     pub fn get_slider_or_queen_defending_square_location(
@@ -97,6 +97,104 @@ impl Board {
         if location2 != -1 {
             result.push(location2);
         }
+    }
+
+    pub fn get_pieces_defending_square_on_northeast_southwest_diagonal_locations(
+        &mut self,
+        square: i8,
+        defended_piece_location: i8,
+        defended_color: Color,
+        result: &mut Vec<i8>,
+    ) {
+        let colorized_bishop = colorize_piece(BISHOP, defended_color);
+        let location1 = self.get_slider_or_queen_defending_square_location(
+            square,
+            colorized_bishop,
+            defended_piece_location,
+            defended_color,
+            9,
+            |attacker_square, square_file| {
+                attacker_square < 64 && attacker_square & 7 > square_file
+            },
+        );
+
+        if location1 != -1 {
+            result.push(location1);
+        }
+
+        let location2 = self.get_slider_or_queen_defending_square_location(
+            square,
+            colorized_bishop,
+            defended_piece_location,
+            defended_color,
+            -9,
+            |attacker_square, square_file| {
+                attacker_square >= 0 && attacker_square & 7 < square_file
+            },
+        );
+
+        if location2 != -1 {
+            result.push(location2);
+        }
+    }
+
+    pub fn get_pieces_defending_square_on_northwest_southeast_diagonal_locations(
+        &mut self,
+        square: i8,
+        defended_piece_location: i8,
+        defended_color: Color,
+        result: &mut Vec<i8>,
+    ) {
+        let colorized_bishop = colorize_piece(BISHOP, defended_color);
+        let location1 = self.get_slider_or_queen_defending_square_location(
+            square,
+            colorized_bishop,
+            defended_piece_location,
+            defended_color,
+            7,
+            |attacker_square, square_file| {
+                attacker_square < 64 && attacker_square & 7 < square_file
+            },
+        );
+
+        if location1 != -1 {
+            result.push(location1);
+        }
+
+        let location2 = self.get_slider_or_queen_defending_square_location(
+            square,
+            colorized_bishop,
+            defended_piece_location,
+            defended_color,
+            -7,
+            |attacker_square, square_file| {
+                attacker_square >= 0 && attacker_square & 7 > square_file
+            },
+        );
+
+        if location2 != -1 {
+            result.push(location2);
+        }
+    }
+    pub fn get_pieces_defending_square_on_diagonals_locations(
+        &mut self,
+        square: i8,
+        defended_piece_location: i8,
+        defended_color: Color,
+        result: &mut Vec<i8>,
+    ) {
+        self.get_pieces_defending_square_on_northwest_southeast_diagonal_locations(
+            square,
+            defended_piece_location,
+            defended_color,
+            result,
+        );
+        self.get_pieces_defending_square_on_northeast_southwest_diagonal_locations(
+            square,
+            defended_piece_location,
+            defended_color,
+            result,
+        );
     }
 
     pub fn get_sliders_or_queens_defending_square_on_straight_lines_locations(
