549770025c29baf1ba9bc252ec28bdf7196a4537
ikcilrep <frajerzycki@protonmail.com>
2020-06-16 18:26:07 +0200

    Generate random getting out of check on file moves and fix on rank

--- src/board/legality/getting_out_of_check/move_generation.rs
+++ src/board/legality/getting_out_of_check/move_generation.rs
@@ -8,6 +8,67 @@ use rand::rngs::ThreadRng;
 use rand::Rng;
 
 impl Board {
+    fn generate_random_out_of_check_on_file_move(
+        &mut self,
+        king_location: i8,
+        king_attacker_location: i8,
+        rng: &mut ThreadRng,
+    ) -> Move {
+        let (start, min, max) = if king_attacker_location > king_location {
+            (
+                rng.gen_range(0, king_attacker_location - king_location),
+                king_location + 8,
+                king_attacker_location,
+            )
+        } else {
+            (
+                rng.gen_range(0, king_location - king_attacker_location),
+                king_attacker_location,
+                king_location - 8,
+            )
+        };
+
+        let i_upper_limit = max - min + 1;
+        let mut i = start;
+
+        let defender_locations_getters = [
+            Board::get_sliders_or_queens_defending_square_on_rank_locations,
+            Board::get_pieces_defending_square_on_diagonals_locations,
+        ];
+
+        let mut defender_locations = Vec::new();
+
+        let indexes = {
+            let index1 = rng.gen_range(0, 2) as usize;
+            let index2 = (index1 + 1) & 1;
+            [index1, index2]
+        };
+
+        while {
+            let square = min + i;
+            for &index in indexes.iter() {
+                defender_locations_getters[index](
+                    self,
+                    square,
+                    king_location,
+                    self.state.side,
+                    &mut defender_locations,
+                );
+                if defender_locations.len() > 0 {
+                    let location_index = rng.gen_range(0, defender_locations.len());
+                    let location = defender_locations[location_index];
+                    return new_move(location as usize, square, self);
+                }
+            }
+
+            i += 8;
+            i %= i_upper_limit;
+            i != start
+        } {}
+
+        NULL_MOVE
+    }
+
     fn generate_random_out_of_check_on_rank_move(
         &mut self,
         king_location: i8,
@@ -28,7 +89,7 @@ impl Board {
             )
         };
 
-        let squares_between_count = max - min;
+        let i_upper_limit = max - min + 1;
         let mut i = start;
 
         let defender_locations_getters = [
@@ -61,9 +122,8 @@ impl Board {
                 }
             }
 
-            defender_locations.clear();
             i += 1;
-            i %= squares_between_count;
+            i %= i_upper_limit;
             i != start
         } {}
 
@@ -103,7 +163,11 @@ impl Board {
                     rng,
                 )
             } else if attacker_location_file == king_location_file {
-                NULL_MOVE
+                self.generate_random_out_of_check_on_file_move(
+                    king_location,
+                    attacker_location,
+                    rng,
+                )
             } else if difference % 9 == 0 {
                 NULL_MOVE
             } else if difference % 7 == 0 {
