02c4b2c095002796e793d513f4adb7daa3032367
ikcilrep <frajerzycki@protonmail.com>
2020-06-16 19:05:23 +0200

    Split getting_out_of_check.move_generation module

--- src/board/legality/getting_out_of_check/move_generation/straight_lines.rs
+++ src/board/legality/getting_out_of_check/move_generation/straight_lines.rs
@@ -3,12 +3,11 @@ extern crate rand;
 use crate::board::Board;
 use crate::moves::constructors::new_move;
 use crate::moves::{Move, NULL_MOVE};
-use crate::pieces::king;
 use rand::rngs::ThreadRng;
 use rand::Rng;
 
 impl Board {
-    fn generate_random_out_of_check_on_file_move(
+    pub fn generate_random_out_of_check_on_file_move(
         &mut self,
         king_location: i8,
         king_attacker_location: i8,
@@ -69,7 +68,7 @@ impl Board {
         NULL_MOVE
     }
 
-    fn generate_random_out_of_check_on_rank_move(
+    pub fn generate_random_out_of_check_on_rank_move(
         &mut self,
         king_location: i8,
         king_attacker_location: i8,
@@ -127,55 +126,6 @@ impl Board {
             i != start
         } {}
 
-        NULL_MOVE
-    }
-
-    fn generate_random_out_of_check_move(
-        &mut self,
-        king_attackers_locations: Vec<i8>,
-        rng: &mut ThreadRng,
-    ) -> Move {
-        let color = self.state.side;
-        let king_location = self.state.king_positions[color as usize];
-        let legal_moves_to = king::get_legal_moves_to(king_location as usize, self);
-        if legal_moves_to.len() > 0 {
-            return king::generate_random_getting_of_check_move(
-                king_location as usize,
-                &legal_moves_to,
-                self,
-                rng,
-            );
-        }
-
-        if king_attackers_locations.len() == 1 {
-            let attacker_location = king_attackers_locations[0];
-            let attacker_location_rank = attacker_location >> 3;
-            let king_location_rank = king_location >> 3;
-            let attacker_location_file = attacker_location & 7;
-            let king_location_file = king_location & 7;
-
-            let difference = attacker_location - king_location;
-
-            return if attacker_location_rank == king_location_rank {
-                self.generate_random_out_of_check_on_rank_move(
-                    king_location,
-                    attacker_location,
-                    rng,
-                )
-            } else if attacker_location_file == king_location_file {
-                self.generate_random_out_of_check_on_file_move(
-                    king_location,
-                    attacker_location,
-                    rng,
-                )
-            } else if difference % 9 == 0 {
-                NULL_MOVE
-            } else if difference % 7 == 0 {
-                NULL_MOVE
-            } else {
-                NULL_MOVE
-            };
-        }
         NULL_MOVE
     }
 }
