8142600bca5a64661e3ed6f642505a8f82487495
ikcilrep <frajerzycki@protonmail.com>
2020-06-28 00:17:00 +0200

    Fix generating out of check moves on file

--- src/board/legality/getting_out_of_check/move_generation/straight_lines.rs
+++ src/board/legality/getting_out_of_check/move_generation/straight_lines.rs
@@ -2,7 +2,6 @@ extern crate rand;
 
 use crate::board::Board;
 use crate::moves::Move;
-use crate::pieces::color::Color;
 use rand::rngs::ThreadRng;
 
 impl Board {
@@ -17,7 +16,7 @@ impl Board {
             Board::get_sliders_or_queens_defending_square_on_rank_locations,
             Board::get_pieces_defending_square_on_diagonals_locations,
             Board::get_knights_defending_square_locations,
-            |_: &mut Board, _: i8, _: i8, _: Color, _: &mut Vec<i8>| {},
+            Board::get_pawns_defending_square_locations,
         ];
 
         self.generate_random_out_of_specific_check_move(
