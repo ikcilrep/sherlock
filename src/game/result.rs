d38ad966eaad7d17bc1bb2a0cdf7d998d1161634
ikcilrep <frajerzycki@protonmail.com>
2020-06-28 00:44:38 +0200

    Add getting points of game result

--- src/game/result.rs
+++ src/game/result.rs
@@ -5,3 +5,13 @@ pub enum GameResult {
     Win(Color),
     StillInProgress,
 }
+
+impl GameResult {
+    pub fn get_points(self, side: Color) -> i32 {
+        match self {
+            GameResult::Draw => 1,
+            GameResult::Win(color) => return if color == side { 2 } else { 0 },
+            GameResult::StillInProgress => panic!("Can't get points of unfinished game."),
+        }
+    }
+}
