32f41f702dc0647e686279d8891238568f1d2129
ikcilrep <frajerzycki@protonmail.com>
2020-06-28 22:55:06 +0200

    Add finding best move

--- src/game/probability.rs
+++ src/game/probability.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 use crate::board::Board;
 use crate::game::play_random_game;
+use crate::moves::Move;
 use crate::pieces::color::Color;
 use rand::rngs::ThreadRng;
 
@@ -12,5 +13,21 @@ impl Board {
             result += play_random_game(&mut self_clone, rng).get_points(color);
         }
         result
+    }
+
+    pub fn get_best_move(&mut self, repetitions: i32, rng: &mut ThreadRng) -> Move {
+        let moves = self.generate_all_legal_moves();
+        let mut best_score = -1;
+        let mut best_move = *moves.iter().next().unwrap();
+        for half_move in moves {
+            let mut self_clone = self.clone();
+            self_clone.make_move(half_move);
+            let score = self_clone.get_position_score(self.state.side, repetitions, rng);
+            if score > best_score {
+                best_score = score;
+                best_move = half_move;
+            }
+        }
+        best_move
     }
 }
