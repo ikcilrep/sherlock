651cb350a82370f6c228cf3a235f749646cd5556
ikcilrep <frajerzycki@protonmail.com>
2020-06-28 22:55:37 +0200

    Add playing game

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,60 @@ mod board;
 mod game;
 mod moves;
 mod pieces;
+
+use crate::pieces::color::WHITE;
 use crate::board::Board;
-use crate::game::play_random_game;
+use crate::game::result::GameResult;
+use crate::moves::algebraic_notation::from_algebraic_notation;
+use crate::moves::algebraic_notation::to_algebraic_notation;
 
 fn main() {
-    let mut board = Board::new();
     let mut rng = rand::thread_rng();
+    let mut board = Board::new();
+    let mut counter = 1;
+    loop {
+        let half_move = board.get_best_move(1000, &mut rng);
+        board.make_move(half_move);
+        println!(
+            "{}.\t{}",
+            counter,
+            to_algebraic_notation(half_move, &mut board)
+        );
 
-    play_random_game(&mut board, &mut rng);
+        let king_attackers_locations =
+            board.get_attackers_of_king_square_locations(board.state.side);
+
+        match board.get_game_state(&king_attackers_locations) {
+            GameResult::StillInProgress => {}
+            GameResult::Win(color) => {
+                if color == WHITE {
+                    println!("White won!");
+                } else {
+                    println!("Black won!");
+                }
+                break;
+            }
+            GameResult::Draw => {
+                println!("Draw!");
+                break;
+            }
+        }
+        let mut opponent_move_str = String::new();
+        loop {
+            match std::io::stdin().read_line(&mut opponent_move_str) {
+                Ok(_) => {
+                    opponent_move_str.pop();
+                    match from_algebraic_notation(&opponent_move_str, &mut board) {
+                        Some(opponent_move) => {
+                            board.make_move(opponent_move);
+                            break;
+                        }
+                        None => println!("Incorrect move, try again."),
+                    }
+                }
+                Err(message) => println!("{}", message),
+            }
+        }
+        counter += 1;
+    }
 }
